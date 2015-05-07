e8b97c537ebbfedf401ecb155db897eb35b14726
vivianmahr <vivianmahr@gmail.com>
2015-05-06 19:06:31 -0700

    Config JS

---
+++ web/roulette/config/script/config.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#submitbutton").click(function () {
+        window.location = "../search/search.html";
+        
+        // Later, take inputs and pass to backend
+    });
+});
