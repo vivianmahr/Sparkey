d2b298019f7c064aa3c1d3ef409278ffcff90f52
Francis Lin <fracanl@gmail.com>
2015-05-31 22:03:39 -0700

    Set up models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/roulette'
+  get 'static_pages/submit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'static_pages#splash'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -14,6 +19,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :users
+  resources :posts
   # Example resource route with options:
   #   resources :products do
   #     member do
