b2d0af4aba73f5065c644bad4beed0a9bc167b75
Francis Lin <fracanl@gmail.com>
2015-06-06 19:55:47 -0700

    Backend functionality in progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+	def post
+		@posts = Post.all
+		@vibes = Vibe.all
+
+		@posts_with_vibes = Post.find_by_sql "SELECT p.id, p.text_content, v.vibe FROM posts p, vibes v WHERE p.id = v.post_id"
+	end
+
 end
--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -1,6 +1,11 @@
 class RoulettesController < ApplicationController
 	
 	def results
-
+		@text_val = params[:text]
+		@images_val = params[:images]
+		@video_val = params[:video]
+		@vibes_list = params[:vibes]
+		@vibes_list = @vibes_list.gsub(" ", "")
+		@vibes_list = @vibes_list.split(",")
 	end
 end
