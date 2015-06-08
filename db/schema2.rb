34a9663a971ba9e24959c83d2fcaa26930a15ffd
Vivian Mahr <vivianmahr@gmail.com>
2015-06-08 14:30:49 -0700

    DB update

--- db/schema2.rb
+++ db/schema2.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608014512) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150605023932) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text_content"
@@ -34,8 +31,6 @@ ActiveRecord::Schema.define(version: 20150608014512) do
     t.datetime "updated_at",               null: false
     t.decimal  "score"
     t.integer  "views"
-    t.string   "post_type"
-    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,8 +52,8 @@ ActiveRecord::Schema.define(version: 20150608014512) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vibes", force: :cascade do |t|
     t.string   "vibe",       limit: 20
@@ -68,7 +63,15 @@ ActiveRecord::Schema.define(version: 20150608014512) do
     t.datetime "updated_at",            null: false
   end
 
-  add_index "vibes", ["post_id"], name: "index_vibes_on_post_id", using: :btree
-  add_index "vibes", ["user_id"], name: "index_vibes_on_user_id", using: :btree
+  add_index "vibes", ["post_id"], name: "index_vibes_on_post_id"
+  add_index "vibes", ["user_id"], name: "index_vibes_on_user_id"
+
+  create_table "widgets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
