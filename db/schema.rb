c78fe77da847c34a1da73f4b1e043417bca8bd6b
Vivian Mahr <vivianmahr@gmail.com>
2015-06-08 21:31:49 -0700

    Browse

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608014512) do
+ActiveRecord::Schema.define(version: 20150609041410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150608014512) do
     t.integer  "views"
     t.string   "post_type"
     t.string   "description"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
